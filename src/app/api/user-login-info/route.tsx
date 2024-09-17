6858eac9d097b376a7fe9a4f8382387c9af914d1
Coder-Jon014 <jon.coombs14@gmail.com>
2024-09-17 17:52:45 -0500

    Add User Login Info

--- src/app/api/user-login-info/route.tsx
+++ src/app/api/user-login-info/route.tsx
@@ -3,9 +3,14 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
+  const func = searchParams.get('function');
   const userId = searchParams.get('user_id');
 
-  const res = await fetch(`http://localhost:5000/user-login-info?user_id=${userId}`);
+  let apiUrl = 'http://localhost:5000';
+  if (func === 'UserLoginInfoGetRequest') {
+    apiUrl += `/user-login-info?user_id=${userId}`;
+  }
+  const res = await fetch(apiUrl);
   const data = await res.json();
 
   return NextResponse.json(data);
